a247b845db2fef8dae46242b010345124426dd14
Rinaroar <marina.luxin@gmail.com>
2020-07-09 16:47:24 +0200

    added random data to the dropdown

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './App.scss';
 import Dropdown from './Dropdown';
 
+const items = [
+  {
+    id: 1,
+    value: 'Pulp Fiction',
+  },
+  {
+    id: 2,
+    value: 'The Prestige',
+  },
+  {
+    id: 3,
+    value: 'Blade Runner 2049',
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Dropdown />
+        <Dropdown title="Select a movie"  items={items}/>
       </header>
     </div>
   );
